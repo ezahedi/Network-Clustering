2c860cba693f0ef824721563e06687d2d62180c9
EMAD ZAHEDI <zahediem@msu.edu>
2015-11-03 20:37:27 -0500

    Update Single-linkage clustering.py

--- Hierarchical-clustering/Single-linkage clustering.py
+++ Hierarchical-clustering/Single-linkage clustering.py
@@ -1,34 +1,15 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Oct 20 22:28:20 2015
-
-@authors: Emad Zahedi & Vahid Mirjalili
+@author: Emad Zahedi & Vahid Mirjalili
+Input: A graph G.
+Output: Clusters obtained by Average linkage method pluse drawn colored graphs 
+Note: If you are looking for specific level, like 'k', of clusters remove the for loop and initiate Iteration = 'k'
 """
-
-
-# -*- coding: utf-8 -*-
-
-import math
-import random
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import scipy.io
-import sys
-from networkx.utils import dict_to_numpy_array
-from collections import defaultdict
-import time
-import operator
-import matplotlib.pyplot as pyplot
-import copy
-import pandas
 import pylab
-random.seed(0)
 
 
-
-
-Iterations = 3
 G=nx.Graph()
 
 G.add_edge('a','b',weight=0.6)
@@ -37,7 +18,6 @@ G.add_edge('c','d',weight=0.1)
 G.add_edge('c','e',weight=0.7)
 G.add_edge('c','f',weight=0.9)
 G.add_edge('a','d',weight=0.3)
-
 
 val_map = {'A': 1.0,
                    'D': 0.5714285714285714,
@@ -53,68 +33,49 @@ nx.draw(G,pos, node_color = values, node_size=15,edge_color=edge_colors,edge_cma
 pylab.show()
 
 
-
-D_Matrix = nx.floyd_warshall_numpy(G) 
-nodes_label = []
 for i in range(len(G.nodes())):
-     nodes_label.append(set(G.nodes()[i]))
-A = np.vstack([D_Matrix, nodes_label])
+    print("in level :",i)  
 
-
-def _Index(Matrix, a):
-    for i in range(Matrix.shape[0]):
-        for j in range(Matrix.shape[1]):
-            if Matrix[i,j] == a:
-                #print('(Matrix.shape[0])',(Matrix.shape[0]))
-                #print('(Matrix.shape[1])',(Matrix.shape[1]))
-                return(i,j)
-
-
-
-
-for iter in range(Iterations):
-    D_matrix = A[:-1,:]
-    minval = np.min(D_matrix[np.nonzero(D_matrix)])
-    i,j = _Index(D_matrix, minval)
+    Iterations = i
+    D_Matrix = nx.floyd_warshall_numpy(G) 
+    nodes_label = []
+    for i in range(len(G.nodes())):
+         nodes_label.append(set(G.nodes()[i]))
+    A = np.vstack([D_Matrix, nodes_label])
+      
+    def _Index(Matrix, a):
+        for i in range(Matrix.shape[0]):
+            for j in range(Matrix.shape[1]):
+                if Matrix[i,j] == a:
+                    return(i,j)
+       
+    for iter in range(Iterations):
+        D_matrix = A[:-1,:]
+        minval = np.min(D_matrix[np.nonzero(D_matrix)])
+        i,j = _Index(D_matrix, minval)
+        for k in range(D_matrix.shape[1]):
+            A[i,k] = min(A[i,k] , A[j, k])
+            A[k,i] = min(A[k,i] , A[k, j])
+            A[i,i] = 0.0
+            A[-1,:][0,i] = (A[-1,:][0,i]) | (A[-1,:][0,j]) 
+        A = np.delete(A, j, 0)
+        A = np.delete(A, j, 1)
     
-    print('(Matrix.shape[1])',D_matrix.shape)
-    print(D_matrix)
-    print(minval)
-    print( i,j)
-    for k in range(D_matrix.shape[1]):
-        A[i,k] = min(A[i,k] , A[j, k])
-        A[k,i] = min(A[k,i] , A[k, j])
-        A[i,i] = 0.0
-        A[-1,:][0,i] = (A[-1,:][0,i]) | (A[-1,:][0,j]) 
-    A = np.delete(A, j, 0)
-    A = np.delete(A, j, 1)
-
-
-pos=nx.spring_layout(G) # positions for all nodes
-node_colors = ['b','g','r','y','c','k','m'] 
-# nodes
-
-
-C_list = A[-1,:] 
-for Clust in range(C_list.shape[1]):
-        nx.draw_networkx_nodes(G,pos,
-                               nodelist = list(C_list[0,Clust]),
-                               node_color=node_colors[Clust],
-                               node_size=80,
-                           alpha=0.8)
-
-
-# edges
-nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
-nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
-
-
-# some math labels
-labels={}
-for i in range(len(G.nodes())):
-    labels[i]= G.nodes()[i]
-
-
-plt.axis('off')
-plt.savefig("labels_and_colors.png") # save as png
-plt.show() # display
+    print('Clusters are :',np.array(A[-1,:]))
+    pos=nx.spring_layout(G) # positions for all nodes
+    node_colors = ['b','g','r','y','c','k','m'] 
+        
+    C_list = A[-1,:] 
+    for Clust in range(C_list.shape[1]):
+            nx.draw_networkx_nodes(G,pos,
+                                   nodelist = list(C_list[0,Clust]),
+                                   node_color=node_colors[Clust],
+                                   node_size=80,
+                               alpha=0.8)
+    
+    nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
+    nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+    
+    plt.axis('off')
+    plt.savefig("labels_and_colors.png") # save as png
+    plt.show() # display
