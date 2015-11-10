ae5125e2c11d90f44a1176a1ca1c6b95d434d5d0
ezahedi <zahediem@msu.edu>
2015-11-09 23:25:35 -0500

    Hello world

---
+++ Spectral-clustering/Spectral_Clustering.py
@@ -0,0 +1,214 @@
+"""
+@authors: Emad Zahedi and Vahid Mirjalili
+Input : A connected graph G
+Output : n clusters based on Spectral Clustering
+"""
+import scipy.spatial
+import numpy as np
+import networkx as nx
+
+
+n_cluster = 2
+def Laplacian(G):
+    """Finding the laplacian matrix for a graph G"""
+    D = nx.adjacency_matrix(G)   
+    for i in range(len(G.nodes())):
+        Sum = 0
+        for k in range(len(G.nodes())):
+            Sum += D[i,k]
+        D[i,i] = -1*Sum
+    
+    L = -1* D   
+    return(L.todense())
+
+
+def Eigen_valu(G, Matrix):
+    """Creating a dictionary where
+    keys are eigen values 
+    values are corresponding eigen vector"""
+    Eigs = np.linalg.eig(Matrix)  
+    eig_values = Eigs[0] 
+    eig_vectors = Eigs[1] 
+    Dict = {}
+    i = 0
+    while i < len(G.nodes()): 
+        Dict[eig_values[i]] = eig_vectors[:,i] 
+        i += 1
+        
+    return(Dict)
+
+
+def _kmeans_init(X, n_clusters, method='balanced'):
+    """ Initialize k=n_clusters centroids randomly
+    """
+    n_samples = X.shape[0]
+    cent_idx = np.random.choice(n_samples, replace=False, size=n_clusters)
+    
+    centers = X[cent_idx,:]
+    mean_X = np.mean(X, axis=0)
+    
+    if method == 'balanced':
+        centers[n_clusters-1] = n_clusters*mean_X - np.sum(centers[:(n_clusters-1)], axis=0)
+    
+    return (centers)
+
+
+def _assign_clusters(X, centers):
+    """ Assignment Step:
+           assign each point to the closet cluster center
+    """
+    dist2cents = scipy.spatial.distance.cdist(X, centers, metric='seuclidean')
+    membs = np.argmin(dist2cents, axis=1)
+
+    return(membs)
+
+
+def _cal_dist2center(X, center):
+    """ Calculate the SSE to the cluster center
+    """
+    dmemb2cen = scipy.spatial.distance.cdist(X, center.reshape(1,X.shape[1]), metric='seuclidean')
+    return(np.sum(dmemb2cen))
+
+
+def _update_centers(X, membs, n_clusters):
+    """ Update Cluster Centers:
+           calculate the mean of feature vectors for each cluster
+    """
+    centers = np.empty(shape=(n_clusters, X.shape[1]), dtype=float)
+    sse = np.empty(shape=n_clusters, dtype=float)
+    for clust_id in range(n_clusters):
+        memb_ids = np.where(membs == clust_id)[0]
+
+        if memb_ids.shape[0] == 0:
+            memb_ids = np.random.choice(X.shape[0], size=1)
+            #print("Empty cluster replaced with ", memb_ids)
+        centers[clust_id,:] = np.mean(X[memb_ids,:], axis=0)
+        
+        sse[clust_id] = _cal_dist2center(X[memb_ids,:], centers[clust_id,:]) 
+    return(centers, sse)
+
+
+def _kmeans_run(X, n_clusters, max_iter, tol):
+    """ Run a single trial of k-means clustering
+        on dataset X, and given number of clusters
+    """
+    membs = np.empty(shape=X.shape[0], dtype=int)
+    centers = _kmeans_init(X, n_clusters)
+
+    sse_last = 9999.9
+    n_iter = 0
+    for it in range(1,max_iter):
+        membs = _assign_clusters(X, centers)
+        centers,sse_arr = _update_centers(X, membs, n_clusters)
+        sse_total = np.sum(sse_arr)
+        if np.abs(sse_total - sse_last) < tol:
+            n_iter = it
+            break
+        sse_last = sse_total
+
+    return(centers, membs, sse_total, sse_arr, n_iter)
+
+
+def _kmeans(X, n_clusters, max_iter, n_trials, tol):
+    """ Run multiple trials of k-means clustering,
+        and outputt he best centers, and cluster labels
+    """
+    n_samples, n_features = X.shape[0], X.shape[1]
+
+    centers_best = np.empty(shape=(n_clusters,n_features), dtype=float)
+    labels_best  = np.empty(shape=n_samples, dtype=int)
+    for i in range(n_trials):
+        centers, labels, sse_tot, sse_arr, n_iter  = _kmeans_run(X, n_clusters, max_iter, tol)
+        if i==0:
+            sse_tot_best = sse_tot
+            sse_arr_best = sse_arr
+            n_iter_best = n_iter
+            centers_best = centers.copy()
+            labels_best  = labels.copy()
+        if sse_tot < sse_tot_best:
+            sse_tot_best = sse_tot
+            sse_arr_best = sse_arr
+            n_iter_best = n_iter
+            centers_best = centers.copy()
+            labels_best  = labels.copy()
+
+    return(centers_best, labels_best, sse_arr_best, n_iter_best)
+
+
+def Spectral_clustering(G):
+    Matrix = Laplacian(G)
+    Eigs = np.linalg.eig(Matrix) 
+    Sorting = np.sort(Eigs[0])
+    least_eig_valu = Sorting[1] # not zero
+    least_eig_vec = np.array(Eigen_valu(G, Matrix)[least_eig_valu])
+    Classes = list(_kmeans(least_eig_vec, n_cluster, 100, 10, 0.001)[1])
+    return Classes
+
+def getting_index(list):
+    Dict = {}
+    i = 0
+    for a in list:
+        if a not in Dict:
+            Dict[a] = [i]
+        else:
+            Dict[a].append(i)
+        i +=1
+    return(Dict)
+    
+
+##
+## Class Soectral clustering
+##
+class SpecClust_error( ValueError ):
+    
+    pass
+
+##
+## Class Spectral Clustering
+##
+class SpecClust(object):
+    """
+    Spectral Clustering
+    Parameters
+    ----------------
+           G : A given graph
+           n_clusters: number of clusters (default = 2)
+           n_trials: number of trial random centroid initialization (default = 10)
+           max_iter: maximum number of iterations (default = 100)
+           tol: tolerance (default = 0.0001)
+    Attibutes
+    -------
+       Classes_   :  cluster labels for each node
+       centers_  :  cluster centers
+       sse_arr_  :  array of SSE values for each cluster
+       n_iter_   :  number of iterations for the best trial
+
+        Methods
+        ------- 
+           fit(X): fit the model
+           fit_predict(X): fit the model and return the cluster labels
+    """
+    def __init__(self, G, n_clusters=2, n_trials=10, max_iter=100, tol=0.001):
+        self.G         = G
+        self.n_custers = n_clusters
+        self.n_trials  = n_trials
+        self.max_iter  = max_iter
+        self.tol       = tol
+        
+    
+    def fit(self, G):
+        """ Apply Spectral Clustering
+              G: is a given graph
+        """
+        self.Classes = Spectral_clustering(self.G)
+        self.Cluster_for_index = getting_index(self.Classes)
+
+    def fit_predict(self, G):
+        """ Apply Spectral Clustering, 
+            and return clusters
+        """
+        self.fit(G)
+        return(self.Cluster_for_index)
+        
+    def __str__(self):
+        return 'The clusters are: {0}'.format(self.Cluster_for_index)
---
+++ Spectral-clustering/test.py
@@ -0,0 +1,85 @@
+"""The algorithm is designed to find the min cut if you would like to find more
+ clusters you need to change the variable 'n_cluster' iniside the 
+ Spectral_Clustering.py as well.
+ """
+
+import Spectral_Clustering as SCl
+import networkx as nx
+import pylab
+import matplotlib.pyplot as plt
+
+
+n_cluster = 2
+
+G=nx.Graph() #petersonii
+
+G.add_edge('a','b',weight=4)
+G.add_edge('b','c',weight=4)
+G.add_edge('c','d',weight=4)
+G.add_edge('d','e',weight=4)
+G.add_edge('e','a',weight=4)
+
+G.add_edge('f','g',weight=4)
+G.add_edge('g','h',weight=4)
+G.add_edge('h','i',weight=4)
+G.add_edge('i','j',weight=4)
+G.add_edge('j','f',weight=4)
+
+G.add_edge('a','f',weight=.1)
+G.add_edge('b','g',weight=.1)
+G.add_edge('c','h',weight=.1)
+G.add_edge('d','i',weight=.1)
+G.add_edge('e','j',weight=.1)
+
+
+def main(G):
+    """draw the input graph and the colored out put graph
+       determine the clusters after each level of merging
+    """ 
+    try:
+        val_map = {'A': 1.0,
+                           'D': 0.5714285714285714,
+                                      'H': 0.0}
+        values = [val_map.get(node, 0.45) for node in G.nodes()]
+        edge_colors = 'k'
+        
+        edge_labels=dict([((u,v,),d['weight'])
+                     for u,v,d in G.edges(data=True)])
+        pos=nx.spring_layout(G) # positions for all nodes                
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+        nx.draw(G,pos, node_color = values, node_size=15,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
+        pylab.show()
+        
+        SC = SCl.SpecClust(G)
+        pos=nx.spring_layout(G) # positions for all nodes
+        node_colors = ['b','g','r','y','c','k','m'] 
+        
+        Cluster_for_index = SC.fit_predict(G) 
+        
+        for i in range(n_cluster): 
+            Nodes = []
+            for j in Cluster_for_index[i]:
+                Nodes.append(G.nodes()[j])
+                
+                
+                
+            nx.draw_networkx_nodes(G,pos,
+                                   nodelist = Nodes,
+                                   node_color=node_colors[i],
+                                   node_size=80,
+                               alpha=0.8)       
+        edge_colors = 'k'
+        edge_labels=dict([((u,v,),d['weight'])
+                     for u,v,d in G.edges(data=True)])               
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+        nx.draw(G,pos, node_color = values, node_size=1,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
+        pylab.show()
+        
+        print SC.__str__()
+
+
+    except SCl.SpecClust_error:
+        
+        print( "Got an imput error, please change the input and try it again." )
+
+main(G)
